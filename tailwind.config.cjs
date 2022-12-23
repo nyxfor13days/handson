93c913f20f2788909789cc0fa541f5b0b2c27fcd
nyxfor13days <nyxfor13days@gmail.com>
2022-12-23 12:15:22 +0530

    Added content files to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{ts,tsx}', './index.html'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
